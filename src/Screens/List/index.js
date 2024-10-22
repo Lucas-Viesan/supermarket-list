6aae259aef50f57c29ad57cc0e6e30c7bebbeda4
Lucas-Viesan <vieiratm01@hotmail.com>
2024-10-22 11:34:51 -0300

    feat: create final layout for the list

--- src/Screens/List/index.js
+++ src/Screens/List/index.js
@@ -1,15 +1,57 @@
 import "./index.css";
 import { useState, useEffect } from "react";
 import { getList } from "../../services/request";
-import { ListCard } from "../../components";
+import { ListCard, Loader, ListRender, Button } from "../../components";
 
 export const ListScreen = () => {
   const [loading, setLoading] = useState(true);
   const [listData, setListData] = useState([]);
+  const options = [
+    {
+      id: 0,
+      name: "Arroz",
+      quantity: 2,
+    },
+    {
+      id: 1,
+      name: "Feijão",
+      quantity: 3,
+    },
+    {
+      id: 2,
+      name: "Feijão",
+      quantity: 3,
+    },
+    {
+      id: 3,
+      name: "Feijão",
+      quantity: 3,
+    },
+    {
+      id: 4,
+      name: "Feijão",
+      quantity: 3,
+    },
+    {
+      id: 5,
+      name: "Feijão",
+      quantity: 3,
+    },
+    {
+      id: 6,
+      name: "Feijão",
+      quantity: 3,
+    },
+    {
+      id: 7,
+      name: "Feijão",
+      quantity: 3,
+    },
+  ];
 
   const loadListItems = async () => {
     setLoading(true);
-    const result = await getList();
+    const result = options; //await getList();
     console.log({ result });
     setListData(result);
     setLoading(false);
@@ -22,23 +64,21 @@ export const ListScreen = () => {
     <div className="list-screen-container">
       <div className="list-screen-content-container">
         <div className="list-screen-header">
-          <img
-            className="logo-image"
-            src="/images/logo.png"
-            alt="supermarket-list-logo"
-          />
-          <h1>Lista Supermercado</h1>
+          <div className="list-screen-title-container">
+            <img
+              className="logo-image"
+              src="/images/logo.png"
+              alt="supermarket-list-logo"
+            />
+
+            <h1 className="list-screen-header-title">Lista Supermercado</h1>
+          </div>
+          <div className="list-screen-button-container">
+            <Button>Adicionar</Button>
+          </div>
         </div>
         <div className="list-screen-list-container">
-          {loading && <h3>Carregando...</h3>}
-          {!loading && listData?.length > 0 ? (
-            listData.map((item) => <ListCard key={item.id} item={item} />)
-          ) : (
-            <h3>
-              Sua lista está vazia, adicione um novo item clicando no botão de
-              "Adicionar"
-            </h3>
-          )}
+          {loading ? <Loader /> : <ListRender list={listData} />}
         </div>
       </div>
     </div>
--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,5 @@
 export * from "./Button";
 export * from "./Input";
 export * from "./ListCard/ListCard";
+export * from "./Loader";
+export * from "./ListRender";
